24bf4c5af96d2a51f5b12f061c103123ec0f9476
Sameer Khan <sameerkhann241@gmail.com>
2025-05-02 21:14:26 +0530

    Domain setup

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // ✅ Dynamic origin handling
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://navifymodel.netlify.app"
+  "http://navify.co.in"
 ];
 
 const corsOptions = {
